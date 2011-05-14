bb2d8e8a0a3fbe6dfee06344c35fbf40c97adeb3
Claudia Aguilar <claudialorena.aguilar@gmail.com>
2011-05-14 16:00:15 -0500

    Working Post and Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Blog::Application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+
+  get "home/index"
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
